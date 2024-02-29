daa83cfb5ffdc8523ea5652ca5dbb64c4cba00b3
Kyle Laverty <k.laverty@gmail.com>
2024-02-29 15:35:01 -0500

    Part 101 - Add db models

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,9 +1,69 @@
 package models
 
+import "time"
+
 // Reservation holds reservation data.
 type Reservation struct {
 	FirstName string
 	LastName  string
 	Email     string
 	Phone     string
+}
+
+// Users is the model for the user table.
+type Users struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	Email       string
+	Password    string
+	AccessLevel int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+// Rooms is the model for the rooms table.
+type Rooms struct {
+	ID        int
+	RoomName  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// Restrictions is the model for the restrictions table.
+type Restrictions struct {
+	ID          int
+	Restriction string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+// Reservations is the model for the reservations table.
+type Reservations struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Email     string
+	Phone     string
+	StartDate time.Time
+	EndDate   time.Time
+	RoomID    int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Room      Rooms
+}
+
+// RoomRestrictions is the model for the room_restrictions table.
+type RoomRestrictions struct {
+	ID            int
+	StartDate     time.Time
+	EndDate       time.Time
+	RoomID        int
+	ReservationID int
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	Room          Rooms
+	Reservation   Reservations
+	RestrictionID int
+	Restriction   Restrictions
 }
