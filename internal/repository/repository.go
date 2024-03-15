635c3b9258c21ffbea6856329d9679dc36b20360
Kyle Laverty <k.laverty@gmail.com>
2024-03-15 10:06:01 -0400

    Part 102-105 - Add first DB insurt

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,5 +1,9 @@
 package repository
 
+import "github.com/kylelaverty/bookings/internal/models"
+
 type DatabaseRepo interface {
 	AllUsers() bool
+
+	InsertReservation(res models.Reservation) error
 }
